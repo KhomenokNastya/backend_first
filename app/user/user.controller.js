5fa65c06dd0d811b68b36551c5f252faf43cb44c
KhomenokNastya <khomenok.nastya@bk.ru>
2023-06-24 00:05:47 +0300

    Adding statistics: minutes, workout, kgs

--- app/user/user.controller.js
+++ app/user/user.controller.js
@@ -4,7 +4,7 @@ import { prisma } from '../prisma.js'
 import { UserFields } from '../utils/user.utils.js'
 
 // @desc     Get user profile
-// @route    GET /api/user/profile
+// @route    GET /api/users/profile
 // @access   Private
 export const getUserProfile = asyncHandler(async (req, res) => {
 	const user = await prisma.user.findUnique({
@@ -14,5 +14,47 @@ export const getUserProfile = asyncHandler(async (req, res) => {
 		select: UserFields
 	})
 
-	res.json(user)
+	const countExerciseTimesCompleted = await prisma.exerciseLog.count({
+		where: {
+			userId: req.user.id,
+			isCompleted: true
+		}
+	})
+
+	const kgs = await prisma.exerciseTime.aggregate({
+		where: {
+			exerciseLog: {
+				userId: req.user.id
+			},
+			isCompleted: true
+		},
+		_sum: {
+			weight: true
+		}
+	})
+
+	const workouts = await prisma.workoutLog.count({
+		where: {
+			userId: user.id,
+			isCompleted: true
+		}
+	})
+
+	res.json({
+		...user,
+		statistics: [
+			{
+				label: 'Minutes',
+				value: Math.ceil(countExerciseTimesCompleted * 2.3) || 0
+			},
+			{
+				label: 'Workouts',
+				value: workouts
+			},
+			{
+				label: 'Kgs',
+				value: kgs._sum.weight || 0
+			}
+		]
+	})
 })
